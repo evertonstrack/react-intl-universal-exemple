2d55396cc7f302f315d68a58887351fcee7da9e9
evertonstrack <everton.strack@gmail.com>
2018-09-20 15:58:37 -0300

    Post about react-intl-universal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import intl from 'react-intl-universal';
+
 import logo from './logo.svg';
 import './App.css';
 
+
+const locales = {
+  'pt-BR': require('./locales/pt-BR.json'),
+  'en-US': require('./locales/en-US.json')
+};
+
 class App extends Component {
+
+  constructor() {
+    super();
+
+    const currentLocale = locales[navigator.language] ? navigator.language : 'pt-BR';
+
+    intl.init({
+      currentLocale,
+      locales
+    });
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{intl.get('header.title', { nome: 'Everton' })}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {intl.getHTML('home.description', { arquivo: 'src/App.js' })}
         </p>
       </div>
     );
